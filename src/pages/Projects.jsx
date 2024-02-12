b4704f090def2b2fdc8c32ecbd2b3c707a32a305
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-11 20:41:41 -0500

    Started on projects sectioning and css

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,13 @@
 export default function Projects() {
-    return (
-      <div>
-        <h2>Projects Page</h2>
+  return (
+    <div>
+      <h2>Projects</h2>
+      <h3>Group Efforts</h3>
+      <div class="group-img-section">
+        <img src="/src/assets/rooms-sc.png" alt="screenshot of rööms homepage" />
+        <img src="/src/assets/Meal2Reel-Empty-SC.png" alt="screenshot of Meal2Reel homepage" />
       </div>
-    );
-  }
+      <h3>Solo Efforts</h3>
+    </div>
+  );
+}
