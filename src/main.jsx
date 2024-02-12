0e00fba5dc861305b8a52d2ef510946e5aa7e386
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-11 19:04:56 -0500

    Boilerplate to Nav.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,36 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 
-import App from './App'
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Projects from './pages/Projects';
-import Error from './pages/Error';
-
 import './main.css'
 
+import App from './App'
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <Projects />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
-  );  
+  <RouterProvider router={router} />
+);
