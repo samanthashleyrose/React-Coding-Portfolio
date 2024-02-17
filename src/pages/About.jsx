6e8885c31794edef956d2779d05bf63796e49400
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-16 20:02:35 -0500

    Added about me paragraph

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,12 @@
 export default function About() {
-    return (
-      <div>
-        <h2>About</h2>
-        <p id="about-section">This is the about page</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>About</h2>
+      <p id="about-section">With a professional background in customer service and manual labor within a warehouse setting, I have recently transitioned into the realm of web development. This career change signifies a necessary step in my professional journey. Embracing coding as a new endeavor, I approach it with both eagerness to learn and a determination to excel. <br/>
+      <br/>
+      Navigate to the Projects page to view icons representing projects completed during UConn's FullStack Bootcamp Course, illustrating proficiency in HTML, CSS, JavaScript, Node.js, React, MySql, and NoSQL. To explore each project, click on the image's title to be brought to the deployed application. Additionally, see the GitHub link on the Contact Page for each project's repository information.<br/>
+      <br/>
+      As you navigate through my portfolio, you will observe how mastering the foundational principles and techniques of each technology translates into high-functioning performance and seamless design.</p>
+    </div>
+  );
+}
