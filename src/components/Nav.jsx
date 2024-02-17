1e6b83071a51c501e6c358bbc63324a811fff465
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-16 19:30:20 -0500

    removed unnecessary comments

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,8 +8,6 @@ function Nav() {
             <p className="nav-option">
                 <Link
                     to="/"
-                    // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-                    // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
                     className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
                 >
                     About
@@ -18,7 +16,6 @@ function Nav() {
             <p className="nav-option">
                 <Link
                     to="/Projects"
-                    // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                     className={currentPage === '/Projects' ? 'nav-link active' : 'nav-link'}
                 >
                     Projects
@@ -27,7 +24,6 @@ function Nav() {
             <p className="nav-option">
                 <Link
                     to="/Contact"
-                    // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                     className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
                 >
                     Contact
