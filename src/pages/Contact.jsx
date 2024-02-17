e4c4da3cd42bf7abb3b18cead464258fdc9dc7e4
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-16 20:09:40 -0500

    started on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,9 @@ export default function Contact() {
     return (
       <div>
         <h2>Contact</h2>
-        <p id="contact-section">This is the contact page</p>
+        <p className="contact-section">Email: samantha.rose327@gmail.com</p>
+        <p className="contact-section"><a href="https://github.com/samanthashleyrose">GitHub: samanthashleyrose</a></p>
+        <a className="contact-section">LinkedIn: Coming Soon</a>
       </div>
     );
   }
