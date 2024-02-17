fed8cf6a393e48dba8e3631fbff665342ddd3a30
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-17 10:05:16 -0500

    Added additional projects imgs and updated css

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,10 +4,20 @@ export default function Projects() {
       <h2>Projects</h2>
       <h3>Group Efforts</h3>
       <div class="group-img-section">
-        <img src="/src/assets/rooms-sc.png" alt="screenshot of rööms homepage" />
-        <img src="/src/assets/Meal2Reel-Empty-SC.png" alt="screenshot of Meal2Reel homepage" />
+        <img src="/src/assets/imgs/rooms-sc.png" alt="Screenshot of rööms homepage" />
+        <img src="/src/assets/imgs/Meal2Reel-Empty-SC.png" alt="Screenshot of Meal2Reel homepage" />
       </div>
       <h3>Solo Efforts</h3>
+      <div class="solo-img-row-1">
+      <img src="/src/assets/imgs/LeeQuotes.com-SC.png" alt="Screenshot of LeeQuotes.com" />
+      <img src="/src/assets/imgs/Mandela-Effect-Challenge.png" alt="Screenshot of Mandela Effect Challenge" />
+      </div>
+      <br />
+      <br />
+      <div class="solo-img-row-2">
+      <img src="/src/assets/imgs/README-gen-SC.png" alt="Screenshot of README Generator" />
+      <img src="/src/assets/imgs/employee-tracker.png" alt="Screenshot of Employee Generator" />
+      </div>
     </div>
   );
 }
