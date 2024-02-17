b477f6080bc81c25c2e0dd21aecea354468ba645
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-17 15:39:16 -0500

    Worked on downloadable resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,19 @@
 export default function Resume() {
-    return (
-      <div>
-        <h2>Resume</h2>
-        <p id="resume-section">This is the resume page</p>
+  return (
+    <div>
+      <h2>Resume</h2>
+      <div className="resume-section">
+        <h3>Proficiencies</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+        </ul>
+        <p id="download-resume">Download my resume: <a href="/src/assets/RESUME-2024-Samantha-Rose.pdf" download>Resume.pdf</a></p>
       </div>
-    );
-  }
+    </div>
+  );
+}
