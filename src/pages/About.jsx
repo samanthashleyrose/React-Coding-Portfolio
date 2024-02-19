fdaf36e34963e2cec715436731b20991bb130bc2
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-19 13:06:29 -0500

    Added pfp

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,11 +2,14 @@ export default function About() {
   return (
     <div>
       <h2>About</h2>
-      <p id="about-section">With a professional background in customer service and manual labor within a warehouse setting, I have recently transitioned into the realm of web development. This career change signifies a necessary step in my professional journey. Embracing coding as a new endeavor, I approach it with both eagerness to learn and a determination to excel. <br/>
+      <div className="about-section">
+      <p id="about-me">With a professional background in customer service and manual labor within a warehouse setting, I have recently transitioned into the realm of web development. This career change signifies a necessary step in my professional journey. Embracing coding as a new endeavor, I approach it with both eagerness to learn and a determination to excel. <br/>
       <br/>
       Navigate to the Projects page to view icons representing projects completed during UConn's FullStack Bootcamp Course, illustrating proficiency in HTML, CSS, JavaScript, Node.js, React, MySql, and NoSQL. To explore each project, click on the image's title to be brought to the deployed application. Additionally, see the GitHub link on the Contact Page for each project's repository information.<br/>
       <br/>
       As you navigate through my portfolio, you will observe how mastering the foundational principles and techniques of each technology translates into high-functioning performance and seamless design.</p>
+      <img src="/src/assets/imgs/pfp.png" alt="Samantha Rose Profile Photo" id="pfp"/>
+      </div>
     </div>
   );
 }
