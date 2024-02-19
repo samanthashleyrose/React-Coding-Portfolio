b5983169fd202edd32e05605548fc3941279b06d
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-19 15:00:58 -0500

    Img path adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
         <div className="footer">
             <h4>made with ❤</h4>
             <a href="https://github.com/samanthashleyrose?tab=repositories">
-                <img src="/public/assets/imgs/github-icon.png" alt="Github Icon" className="footer-option"/>
+                <img src="/assets/imgs/github-icon.png" alt="Github Icon" className="footer-option"/>
             </a>
             <a href="">
-                <img src="/public/assets/imgs/linkedin-icon.png" alt="LinkedIn Icon" className="footer-option"/>
+                <img src="/assets/imgs/linkedin-icon.png" alt="LinkedIn Icon" className="footer-option"/>
             </a>
             <a href="samantha.rose327@gmail.com">
-                <img src="/public/assets/imgs/gmail-icon.png" alt="Gmail Icon" className="footer-option"/>
+                <img src="/assets/imgs/gmail-icon.png" alt="Gmail Icon" className="footer-option"/>
             </a>
         </div>
     );
