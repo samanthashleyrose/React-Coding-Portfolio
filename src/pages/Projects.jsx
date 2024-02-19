666781170e4b23475ecbef21b53c26679733b4c4
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-19 18:00:42 -0500

    Added repo links

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,12 +7,14 @@ export default function Projects() {
       <div className="group-img-section">
         <div id='group-img-1'>
           <div className='img-title'>
-            <a href="https://rooms-3226fb27d847.herokuapp.com/login">rooms</a>
+            <a href="https://rooms-3226fb27d847.herokuapp.com/login">view rooms</a>
+            <a href="https://github.com/samanthashleyrose/rooms">view repo</a>
           </div>
         </div>
         <div id='group-img-2'>
           <div className='img-title'>
             <a href="https://samanthashleyrose.github.io/Meal2Reel/">Meal2Reel</a>
+            <a href="https://github.com/samanthashleyrose/Meal2Reel">view repo</a>
           </div>
         </div>
       </div>
@@ -21,11 +23,13 @@ export default function Projects() {
         <div id='solo-img-1'>
           <div className='img-title'>
             <a href="https://samanthashleyrose.github.io/Mandela-Effect-Challenge/">Mandela Effect Challenge</a>
+            <a href="https://github.com/samanthashleyrose/Mandela-Effect-Challenge">view repo</a>
           </div>
         </div>
         <div id='solo-img-2'>
           <div className='img-title'>
             <a href="https://samanthashleyrose.github.io/LeeQuotes.com/">LeeQuotes.com</a>
+            <a href="https://github.com/samanthashleyrose/LeeQuotes.com">view repo</a>
           </div>
         </div>
       </div>
@@ -35,11 +39,13 @@ export default function Projects() {
         <div id='solo-img-3'>
           <div className='img-title'>
             <a href="https://github.com/samanthashleyrose/README-generator">README Generator</a>
+            <a href="https://github.com/samanthashleyrose/README-generator">view repo</a>
           </div>
         </div>
         <div id='solo-img-4'>
           <div className='img-title'>
             <a href="https://github.com/samanthashleyrose/Employee-Tracker">Employee Tracker Database</a>
+            <a href="https://github.com/samanthashleyrose/Employee-Tracker">view repo</a>
           </div>
         </div>
       </div>
