271008abc463a0441b9d2329abd8dd856cc7beea
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-19 11:27:27 -0500

    Working towards hover over project images

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,7 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h2>Oops!</h2>
+      <h2 id='oops'>Oops!</h2>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
