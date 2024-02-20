d10f007688f18b268c0e6aae52776bd66420846b
samanthashleyrose <samantha.rose327@gmail.com>
2024-02-19 19:47:17 -0500

    Created links to projects and contact page on about page paragraphs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,30 @@
+import { Link, useLocation } from 'react-router-dom';
+
 export default function About() {
+  const currentPage = useLocation().pathname;
+
   return (
     <div>
       <h2>About Me</h2>
       <div className="top-row-about">
-        <p id="about-me"> What should you know about me? I am a capable and consistent problem-solver skilled at prioritizing and managing projects with proficiency. 
-        <br />
-        <br />
-        With a professional background in customer service and manual labor within a warehouse setting, I have recently transitioned into the realm of web development. This career change signifies a necessary step in my professional journey. Embracing coding as a new endeavor, I approach it with both eagerness to learn and a determination to excel.
+        <p id="about-me"> What should you know about me? I am a capable and consistent problem-solver skilled at prioritizing and managing projects with proficiency.
+          <br />
+          <br />
+          With a professional background in customer service and manual labor within a warehouse setting, I have recently transitioned into the realm of web development. This career change signifies a necessary step in my professional journey. Embracing coding as a new endeavor, I approach it with both eagerness to learn and a determination to excel.
         </p>
         <img src="/assets/imgs/pfp.png" alt="Samantha Rose Profile Photo" id="pfp" />
       </div>
       <div className="bottom-row-about">
         <p id="about-me-2">
-          Located on the Projects page are image icons representing projects completed during UConn's FullStack Bootcamp Course. To explore each project, click on the image's title to be brought to the deployed application. Additionally, see the GitHub link on the Contact Page for each project's repository information.<br />
+          Located on the <Link to="/Projects" className={currentPage === '/Projects'} id='projects-link'>Projects</Link> page are image icons representing projects completed during UConn's FullStack Bootcamp Course. To explore each project, click on the image's title to be brought to the deployed application. Additionally, see the GitHub link on the <Link to="/Contact" className={currentPage === '/Contact'} id='contact-link'>Contact</Link> Page for each project's repository information.<br />
           <br />
           As you navigate through my portfolio, you will observe how mastering the foundational principles and techniques of each technology translates into high-functioning performance and seamless design.
         </p>
       </div>
+
+      <p>
+        
+      </p>
     </div>
   );
 }
